79d284c7b05e284b2064d4c67ec16ef69dc4f7d1
daniellecroxton <danikrist@gmail.com>
2019-02-27 16:25:52 -0700

    Done.

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ class BoardMember {
   }
 
   veto() {
-    returns 'No, I must disagree';
+    return 'No, I must disagree';
+  }
+
+  approve() {
+    return 'You can do that!';
+  }
+
+  doCharity() {
+    return 'I like to help people.';
+  }
+
+  releasePressStatement() {
+    return 'You will see great things from Scuber.';
+  }
+
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
   }
 }
 
   class Ceo extends BoardMember {
     hireEmployee() {
-      returns 'Welcome aboard!';
+      return 'Welcome aboard!';
     }
   }
