1845da0005ce3c5faa9f4eec3157a9eb738bb1b9
Daniel Goldberg <danpaulgo@aol.com>
2019-09-08 17:37:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,36 @@
+class BoardMember{
+	constructor(name, homeState, training){
+		this.name = name;
+		this.homeState = homeState;
+		this.training = training
+	}
+
+	approve(){
+		return "You can do that!"
+	}
+
+	veto(){
+		return "No, I must disagree"
+	}
+
+	doCharity(){
+		return "I like to help people."
+	}
+
+	releasePressStatement(){
+		return "You will see great things from Scuber."
+	}
+
+	sayHi(){
+		return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+	}
+
+}
+
+class Ceo extends BoardMember{
+
+	hireEmployee(){
+		return "Welcome aboard!"
+	}
+
+}
